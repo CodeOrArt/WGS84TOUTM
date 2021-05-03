c0d85df38d9194abd66e2e82d5c8f137a7f8912a
zhi <cucxc212@gmail.com>
2021-05-03 11:05:31 +0800

    wgs84转utm

---
+++ converter.py
@@ -0,0 +1,6 @@
+import numpy as np
+import utm
+
+print(utm.from_latlon(40.0691643333333, 116.242161333333))
+# print(utm.to_latlon(-2508762.3877196563, -1688373.3218629, 50, 'N'))
+# utm.from_latlon(np.array([51.2, 49.0]), np.array([7.5, 8.4]))
